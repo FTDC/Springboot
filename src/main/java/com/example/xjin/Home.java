18a38b984886c585ec9bff0009c59bbb2ed02532
xjin <mrtop@live.cn>
2018-07-23 18:06:27 +0800

    springboot 基础web

--- src/main/java/com/example/xjin/Home.java
+++ src/main/java/com/example/xjin/Home.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Home {
 
+	// 测试方法
     @RequestMapping("/demo")
     public String index() {
         return "Hello world";
